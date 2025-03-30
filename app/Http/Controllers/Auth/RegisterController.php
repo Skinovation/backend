450316d1a9f4fde910b80d92701106782b2c385c
Maulidaa <maulidaallya06@gmail.com>
2025-03-30 10:47:47 +0700

    implement JWT authentication

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        $validated['password'] = bcrypt($validated['password']);
+        $user = \App\Models\User::create($validated);
+        $credentials = $request->only('email', 'password');
+        if(!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda salah'
+            ], 401);
+        }
+        $user = auth()->guard('api')->user();
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+            'token' => [
+                'access_token' => $token,
+                'token_type' => 'JWT',
+                'expires_in' => config('jwt.ttl') * 60,
+            ],
+        ]);
+    }
+}
