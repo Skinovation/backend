55808b040d2880f14fd4579c7447b1545d5d7c86
Maulidaa <maulidaallya06@gmail.com>
2025-04-19 23:58:38 +0700

    refactor AnalyzeSkincareController to improve image handling and API response error reporting

--- app/Http/Controllers/Analyze/AnalyzeSkincareController.php
+++ app/Http/Controllers/Analyze/AnalyzeSkincareController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Analyze;
 
 use App\Http\Controllers\BaseController;
+use Illuminate\Support\Facades\Http;
 use Faker\Provider\Base;
 use Illuminate\Http\Request;
 
@@ -23,10 +24,42 @@ class AnalyzeSkincareController extends BaseController
                 return $this->sendError('Gambar atau bahan tidak ditemukan, isi salah satu', [], 422);
             }
 
-            // Simpan gambar ke storage
+            // Simpan gambar ke storage dan ambil path lengkap
             $path = $request->file('image')->store('images');
+            $fullPath = storage_path('app/' . $path);
 
-            return $this->sendResponse(['path' => $path], 'Gambar berhasil diunggah');
+            if(!$request->input('ingredients')) {
+                // Kirim gambar ke API lain
+                $response = Http::attach(
+                    'image', file_get_contents($fullPath), basename($fullPath)
+                )->post('https://huggingface.co/spaces/Maulidaaa/skicare_analyze', [
+                ]);
+
+                if ($response->successful()) {
+                    return $this->sendResponse($response->json(), 'Data berhasil dikirim ke API lain');
+                } else {
+                    return $this->sendError('Gagal mengirim data ke API lain', [
+                        'status' => $response->status(),
+                        'body' => $response->body()
+                    ]);
+                }
+            } else {
+                // Kirim gambar ke API lain
+                $response = Http::attach(
+                    'ingredients', $request->input('ingredients'), basename($fullPath)
+                )->post('https://huggingface.co/spaces/Maulidaaa/skicare_analyze', [
+                ]);
+
+                if ($response->successful()) {
+                    return $this->sendResponse($response->json(), 'Data berhasil dikirim ke API lain');
+                } else {
+                    return $this->sendError('Gagal mengirim data ke API lain', [
+                        'status' => $response->status(),
+                        'body' => $response->body()
+                    ]);
+                }
+            }
+            
         } catch (\Exception $e) {
             return $this->sendError('Gagal mengunggah gambar', ['error' => $e->getMessage()], 500);
         }
